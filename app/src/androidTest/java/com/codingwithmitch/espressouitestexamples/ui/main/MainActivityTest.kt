471aeb55183c0f0eb343d6ab5e06686166758350
Bruno Camargos <brunocamargosn@gmail.com>
2021-11-02 10:55:10 -0300

    apllying simple activity tests

--- app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/ui/main/MainActivityTest.kt
@@ -11,39 +11,26 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
-class MainActivityTest{
+class MainActivityTest {
 
     @Test
-    fun testActivity_inView() {
+    fun test_isActivityInView() {
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-
         onView(withId(R.id.main)).check(matches(isDisplayed()))
     }
 
-    // Visibility
     @Test
-    fun testVisibility_title_nextButton() {
+    fun test_visibility_title_nextButton() {
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-
-        onView(withId(R.id.activity_main_title))
-            .check(matches(isDisplayed())) // method 1
-
-        onView(withId(R.id.activity_main_title))
-            .check(matches(withEffectiveVisibility(Visibility.VISIBLE))) // method 2
-
-        onView(withId(R.id.button_next_activity))
-            .check(matches(isDisplayed()))
+        onView(withId(R.id.button_next_activity)).check(matches(isDisplayed()))
+        onView(withId(R.id.activity_main_title)).check(matches(isDisplayed()))
     }
 
-    // Text
     @Test
-    fun testTitleTextDisplayed() {
+    fun text_isTitleDisplayed() {
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-
-        onView(withId(R.id.activity_main_title))
-            .check(matches(withText(R.string.text_mainactivity)))
+        onView(withId(R.id.activity_main_title)).check(matches(withText(R.string.text_mainactivity)))
     }
-
 }
 
 
