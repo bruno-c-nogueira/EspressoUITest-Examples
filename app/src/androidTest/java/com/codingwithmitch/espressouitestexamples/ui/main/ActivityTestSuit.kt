9b68aee14b0cb54458fe9b918ae53c28b24f7d9b
Bruno Camargos <brunocamargosn@gmail.com>
2021-11-02 21:26:38 -0300

    tests suite

---
+++ app/src/androidTest/java/com/codingwithmitch/espressouitestexamples/ui/main/ActivityTestSuit.kt
@@ -0,0 +1,8 @@
+package com.codingwithmitch.espressouitestexamples.ui.main
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(MainActivityTest::class, SecondaryActivityTest::class)
+class ActivityTestSuit
